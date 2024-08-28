6d85a06b8a461fbfb42edfe1a81f6c6334375828
OpenSailTeam <team@opensail.com>
2024-08-28 11:21:09 -0600

    No commit message

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,6 +6,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+(window as any).App = App;
+
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement!);
 
