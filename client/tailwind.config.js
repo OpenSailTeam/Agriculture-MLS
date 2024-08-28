dd13ba383e345d88c9104d98f4325aeadb3ff03f
OpenSailTeam <team@opensail.com>
2024-08-28 12:40:45 -0600

    Attempt to scope css

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,18 @@
+const { scopedPreflightStyles, isolateInsideOfContainer } = require('tailwindcss-scoped-preflight')
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  important: '#root',
+  important: '#root.tw-preflight',
   theme: {
     extend: {},
-  },
-  corePlugins: {
-
-    preflight: false,
   },
   plugins: [
     require("@xpd/tailwind-3dtransforms"),
     require('tailwindcss/plugin')(({ addVariant }) => {
       addVariant('search-cancel', '&::-webkit-search-cancel-button');
+    }),
+    scopedPreflightStyles({
+      isolationStrategy: isolateInsideOfContainer('.tw-preflight'), // style root name, used to wrap Popper menus etc
     }),
   ],
 };
